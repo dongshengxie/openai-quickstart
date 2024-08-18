f0ffa5887658d6890fd5e536e1429a91bc0f145f
firefox <964333261@qq.com>
2024-08-18 21:09:10 +0800

    feat: gradio用户界面 & 支持多种语言

--- openai-translator/ai_translator/main.py
+++ openai-translator/ai_translator/main.py
@@ -1,11 +1,38 @@
 import sys
 import os
+import gradio as gr
+from utils.logger import Logger
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from utils import ArgumentParser, ConfigLoader, LOG
 from model import GLMModel, OpenAIModel
 from translator import PDFTranslator
+
+def translation(input_file, file_format, target_language, path, pages):
+    LOG.debug(f"[翻译任务]\n源文件: {input_file.name}\n目标语言: {target_language}\n路径: {path}\n页数: {pages}")
+    pg = int(pages)
+    output_file_path = Translator.translate_pdf(
+        input_file.name,  file_format = file_format, target_language=target_language, output_file_path = path, pages = pg)
+
+    return output_file_path
+
+def launch_gradio():
+    iface = gr.Interface(
+        fn=translation,
+        title="OpenAI-Translator v2.0",
+        inputs=[
+            gr.File(label="上传PDF文件"),
+            gr.Textbox(label="文件类型"),
+            gr.Textbox(label="目标语言（默认：中文）", placeholder="Chinese", value="Chinese"),
+            gr.Textbox(label="输出路径"),
+            gr.Textbox(label="页数")
+        ],
+        outputs=[
+            gr.File(label="下载翻译文件")
+        ]
+    )
+    iface.launch(share=True)
 
 if __name__ == "__main__":
     argument_parser = ArgumentParser()
@@ -23,5 +50,11 @@ if __name__ == "__main__":
     file_format = args.file_format if args.file_format else config['common']['file_format']
 
     # 实例化 PDFTranslator 类，并调用 translate_pdf() 方法
-    translator = PDFTranslator(model)
-    translator.translate_pdf(pdf_file_path, file_format)
+    global Translator
+    Translator = PDFTranslator(model)
+    # translator.translate_pdf(pdf_file_path, file_format)
+
+    launch_gradio()
+
+
+
---
+++ openai-translator/ai_translator/translator/pdf_response.py
@@ -0,0 +1,5 @@
+class Response:
+    def __init__(self,  translation, type, status):
+        self.translation = translation
+        self.type = type
+        self.status = status
